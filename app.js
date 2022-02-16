85334f6a231bae2f13d3ec19e69ffba697bc78fe
Jayu <jayeshhande00@gmail.com>
2022-02-16 11:54:58 +0530

    edit code

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 let images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg'];
 
-
-
 var i = 0;
 
 let canvas = document.querySelector("#canvas");
 
-let arrows = document.querySelector(".arrow");
+let arrows = document.querySelectorAll(".arrow");
 
 const object =  `url(./images/${images[0]})`;
-
-
 
 canvas.style.background = object;
 
 
+arrows.forEach(function(arrow){
 
-    arrows.addEventListener('click', function (e) {
-        if (e.target.id === "right") {
+    arrow.addEventListener('click', function (e) {
+        if (e.target.id === "left"){
+            console.log("clicked")
             i--;
-            if (i < 0) {
-                i = images.length - 1;
+            if (i < 0){
+                i = images.length -1;
             }
             canvas.style.background = `url(./images/${images[i]})`;
-        } else if (e.target.id === "left") {
+        } else if (e.target.id === "right") {
+            console.log("clicked");
             i++;
-            if (i >= images.length) {
+            if (i >= images.length ){
                 i = 0;
             }
             canvas.style.background = `url(./images/${images[i]})`;
         }
-    });
+    })
+
+});
